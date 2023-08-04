57d600a2535a8f91537db9412880a81cd3ae8c03
UBC Student <fulino@students.cs.ubc.ca>
2023-08-03 21:54:49 -0700

    update

--- project.php
+++ project.php
@@ -1,4 +1,3 @@
-
 <html>
     <head>
         <title> CPSC 304 2023S T2 PHP/Oracle Project</title>
@@ -38,20 +37,11 @@
     <body>
         <h1> Welcome to application portal for the BEST company! </h1>
         <p>pick a option below to get started with</p><hr />
-        <?php
-            if ($c=OCILogon("ora_fulino", "a72201767", "dbhost.students.cs.ubc.ca:1522/stu")) {
-                echo "Successfully connected to Oracle.\n";
-                OCILogoff($c);
-            } else {
-                $err = OCIError();
-                echo "Oracle Connect Error " . $err['message'];
-            }
-        ?>
 
         <div id="group" class="options-employee">
             <button onclick="appliesFor()">Browse Job Listings</button>
             <button>Past Applications</button>
-            <button onclick="upcomingInterviews()">Upcoming Interviews</button>
+            <button>Upcoming Interviews</button>
             <button onclick="acceptDeny()">Accept/Deny Offer</button>
         </div>
         <p id="demo"></p>
@@ -62,6 +52,8 @@
                 var y = document.getElementById("resetJob");
                 var a = document.getElementById("insert");
                 var b = document.getElementById("insertJob");
+                var printJob = document.getElementById("printJob");
+                // printJob.submit();
                 if (x.style.display === "none") {
                     if (show) {
                         a.style.display = "none";
@@ -74,25 +66,7 @@
                 }
                 show = true;
             }
-
-            function upcomingInterviews() {
-                            var x = document.getElementById("reset");
-                            var y = document.getElementById("resetInterview");
-                            var a = document.getElementById("insert");
-                            var b = document.getElementById("insertInterview");
-                            if (x.style.display === "none") {
-                                if (show) {
-                                    a.style.display = "none";
-                                    b.style.display = "none";
-                                }
-                                x.style.display = "block";
-                                y.style.display = "block";
-                            } else {
-                                x.style.display = "none";
-                            }
-                            show = true;
-                        }
-
+            
             function acceptDeny() {
                 var x = document.getElementById("insert");
                 var y = document.getElementById("insertJob");
@@ -111,23 +85,6 @@
                 }
                 show = true;
             }
-            /*
-            function viewInterview() {
-                var x = document.getElementById("update");
-                var a = document.getElementById("insert");
-                var b = document.getElementById("reset");
-                if (x.style.display === "none") {
-                    if (show) {
-                        a.style.display = "none";
-                        b.style.display = "none";
-                    }
-                    x.style.display = "block";
-                } else {
-                    x.style.display = "none";
-                }
-                show = true;
-            }
-            */
         </script>
 
         <form id="reset" style="display: none" method="POST" action="project.php">
@@ -139,12 +96,6 @@
             <input type="hidden" id="resetJobTablesRequest" name="resetJobTablesRequest">
             <p><input type="submit" value="Reset Job" name="resetJob"></p>
         </form>
-
-        <!-- new -->
-        <form id="resetInterview" style="display: none" method="POST" action="project.php">
-                    <input type="hidden" id="resetInterviewTablesRequest" name="resetInterviewTablesRequest">
-                    <p><input type="submit" value="Reset Interview" name="resetInterview"></p>
-                </form>
 
         <form id="insert" style="display: none" method="POST" action="project.php"> <!--refresh page when submitted-->
             <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
@@ -165,16 +116,6 @@
             <input type="submit" value="Insert" name="insertSubmitJob"></p>
         </form>
 
-        <!-- NEW -->
-                <form id="insertInterview" style="display: none" method="POST" action="project.php"> <!--refresh page when submitted-->
-                    <input type="hidden" id="insertInterviewQueryRequest" name="insertInterviewQueryRequest">
-                    Interviewer: <input type="text" name="insInt"> <br /><br />
-                    Interviewee: <input type="text" name="insIntee"> <br /><br />
-                    Date: <input type="text" name="insDate>"> <br /><br />
-
-                    <input type="submit" value="Insert" name="insertSubmitInterview"></p>
-                </form>
-
         <form id="update" style="display: none" method="POST" action="project.php"> <!--refresh page when submitted-->
             <input type="hidden" id="updateQueryRequest" name="updateQueryRequest">
             Old Name: <input type="text" name="oldName"> <br /><br />
@@ -190,17 +131,15 @@
         </form>
 
         <h2>Print Tuples from jobTable</h2>
-        <form method="GET" action="project.php"> <!--refresh page when submitted-->
+        <form id="printJob" method="GET" action="project.php"> <!--refresh page when submitted-->
             <input type="hidden" id="printRequest" name="printRequest">
             <input type="submit" name="printJob"></p>
         </form>
 
-        <h2>Print Tuples from interviewTable</h2>
-                <form method="GET" action="project.php"> <!--refresh page when submitted-->
-                    <input type="hidden" id="printRequest" name="printRequest">
-                    <input type="submit" name="printInterview"></p>
-                </form>
-
+        <!-- <form method="GET" action="jobListing.php">
+            <input type="hidden" name="positionName" value="positionName">
+            <input type="submit">
+        </form> -->
 
         <?php
         //this tells the system that it's no longer just parsing html; it's now parsing PHP
@@ -242,7 +181,7 @@
             OCILogoff($db_conn);
         }
 
-        function printResult($result) {
+        function printResult($result) { 
             echo "<br>Retrieved data from table demoTable:<br>";
             echo "<table>";
             echo "<tr><th>ID</th><th>Name</th></tr>";
@@ -254,34 +193,17 @@
             }
             echo "</table>";
         }
-        function printResultJob($result) {
+        function printResultJob($result) { 
             echo "<br>Retrieved data from table jobTable:<br>";
             echo "<table>";
             echo "<tr><th>Position</th><th>ReferenceID</th><th>Spots Left</th><th>Annual Salary</th><th>Full/Part</th></tr>";
 
             while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                echo "<tr><td>" . '<a href="https://www.students.cs.ubc.ca/~fulino/oracle-test.php?id='.$row['POSITION'].'">'.$row['POSITION']."</a>". "</td><td>" . $row["REFERENCEID"] . "</td><td>" . $row["SPOTS_LEFT"] . "</td><td>" . $row["ANNUAL_SALARY"] . "</td><td>" . $row["WORK_TYPE"] . "</td></tr>"; //or just use "echo $row[0]"
+                echo "<tr><td>" . '<a target = "_blank" href="https://www.students.cs.ubc.ca/~fulino/jobListing.php?posID='.$row['REFERENCEID'].' ">' . $row['POSITION'] . "</a>" . "</td><td>" . $row["REFERENCEID"] . "</td><td>" . $row["SPOTS_LEFT"] . "</td><td>" . $row["ANNUAL_SALARY"] . "</td><td>" . $row["WORK_TYPE"] . "</td></tr>"; //or just use "echo $row[0]"
             }
             //echo "<br>Retrieved data from table demoTable:<br>";
             echo "</table>";
         }
-
-        function printResultInterview($result) {
-                    echo "<br>Retrieved data from table interviewTable:<br>";
-                    echo "<table>";
-                    echo "<tr><th>Interviewer</th><th>Interviewee</th><th>Date</th></tr>";
-
-                    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                        echo "<tr><td>";
-                        echo $row["INTERVIEWER"] . "</td><td>" . $row["INTERVIEWEE"] . $row["DATE"] . "</td></tr>";
-
-
-                    }
-
-                    echo "</table>";
-                    }
-
-
         function handleResetRequest() {
             global $db_conn;
             // Drop old table
@@ -292,51 +214,50 @@
             executePlainSQL("CREATE TABLE demoTable (id int PRIMARY KEY, name char(30))");
             OCICommit($db_conn);
         }
-
-
+        
+        
         function handleResetJobRequest() {
-            $jobTuple1 = array (":bind1" => "Assistant Chef", ":bind2" => "00001", ":bind3" => "5",b":bind4" => "40534", ":bind5" => "Part");
-            $jobTuple2 = array (":bind1" => "Head Janitor", ":bind2" => "00002", ":bind3" => "2",b":bind4" => "49998", ":bind5" => "Full");
-            $jobTuple3 = array (":bind1" => "Software Enginner", ":bind2" => "00003", ":bind3" => "20",b":bind4" => "90615", ":bind5" => "Full");
-            $jobTuple4 = array (":bind1" => "Customer Service Rep", ":bind2" => "00004", ":bind3" => "20",b":bind4" => "62983", ":bind5" => "Full");
-            $jobTuple5 = array (":bind1" => "Database Intern", ":bind2" => "00005", ":bind3" => "30",b":bind4" => "0", ":bind5" => "Part");
-            $jobTuple6 = array (":bind1" => "Marketing Analyst", ":bind2" => "00006", ":bind3" => "10",b":bind4" => "76526", ":bind5" => "Part");
-            $jobTuple7 = array (":bind1" => "Sales Assistant", ":bind2" => "00007", ":bind3" => "15",b":bind4" => "54219", ":bind5" => "Part");
+            $jobTuple1 = array (":bind1" => "Assistant Chef", ":bind2" => "00001", ":bind3" => "5",b":bind4" => "40534", ":bind5" => "Part", 
+                ":bind6" => "<ul> <li>Food Safe Level 3</li> <li> 2 years of kitchen prep </li></ul>", 
+                ":bind7" => "<ul> <li>Kitchen food prep</li> <li> assist in creation of new menu </li></ul>" );
+            $jobTuple2 = array (":bind1" => "Head Janitor", ":bind2" => "00002", ":bind3" => "2",b":bind4" => "49998", ":bind5" => "Full", 
+                ":bind6" => "<ul><li> 2 years of janitoring </li></ul>", 
+                ":bind7" => "<ul><li> cleaning of the entire 30 floor office tower </li></ul>" );
+            $jobTuple3 = array (":bind1" => "Software Enginner", ":bind2" => "00003", ":bind3" => "20",b":bind4" => "90615", ":bind5" => "Full", 
+                ":bind6" => "<ul><li> 100 years of related experience </li></ul>", 
+                ":bind7" => "<ul><li> make whatever that makes big money </li></ul>" );
+            $jobTuple4 = array (":bind1" => "Customer Service Rep", ":bind2" => "00004", ":bind3" => "20",b":bind4" => "62983", ":bind5" => "Full", 
+                ":bind6" => "<ul><li> 2 years of related experience</li></ul>", 
+                ":bind7" => "duti<ul><li> be nice to customers </li></ul>es" );
+            $jobTuple5 = array (":bind1" => "Database Intern", ":bind2" => "00005", ":bind3" => "30",b":bind4" => "0", ":bind5" => "Part", 
+                ":bind6" => "<ul><li> none required </li></ul>", 
+                ":bind7" => "<ul><li> learn and not get paid unlucky </li></ul>" );
+            $jobTuple6 = array (":bind1" => "Marketing Analyst", ":bind2" => "00006", ":bind3" => "10",b":bind4" => "76526", ":bind5" => "Part", 
+                ":bind6" => "<ul><li> 2 years of related experience </li></ul>", 
+                ":bind7" => "<ul><li> analyze the market </li></ul>" );
+            $jobTuple7 = array (":bind1" => "Sales Assistant", ":bind2" => "00007", ":bind3" => "15",b":bind4" => "54219", ":bind5" => "Part", 
+                ":bind6" => "<ul><li> 2 years of related experience </li></ul>", 
+                ":bind7" => "duti<ul><li> assist in sales? </li></ul>es" );
             $allJobtuples = array ($jobTuple1, $jobTuple2, $jobTuple3, $jobTuple4, $jobTuple5, $jobTuple6, $jobTuple7);
             global $db_conn;
             // Drop old table
             executePlainSQL("DROP TABLE jobTable");
-            echo "<br> old job table dropped <br>";
+            executePlainSQL("DROP TABLE storeTable");
+            executePlainSQL("DROP TABLE coverTable");
+            executePlainSQL("DROP TABLE resumeTable");
+            echo "<br> all old tables dropped <br>";
             // Create new table
-            echo "<br> creating new job table <br>";
-            executePlainSQL("CREATE TABLE jobTable (position char(30), referenceID char(30) PRIMARY KEY, spots_left int, annual_salary int, work_type char(30))");
-            echo "<br> new jobTable created <br>";
-            executeBoundSQL("insert into jobTable values (:bind1, :bind2, :bind3, :bind4, :bind5)", $allJobtuples);
+            echo "<br> creating new tables <br>";
+            executePlainSQL("CREATE TABLE jobTable (position char(30), referenceID char(30) PRIMARY KEY, spots_left int, annual_salary int, work_type char(30), qualification char(100), duty char(100))");
+            executeBoundSQL("insert into jobTable values (:bind1, :bind2, :bind3, :bind4, :bind5, :bind6, :bind7)", $allJobtuples);
             echo "<br> default tuples inserted <br>";
-            OCICommit($db_conn);
+            executePlainSQL("CREATE TABLE storeTable (app_num int PRIMARY KEY, apply_date int)");
+            executePlainSQL("CREATE TABLE coverTable (app_num int PRIMARY KEY, introduction char(300))");
+            executePlainSQL("CREATE TABLE resumeTable (app_num int PRIMARY KEY, name char(30), experience char(300), education char(300))");
+            echo "<br> tables created for store, cover, resume created <br>";
+            OCICommit($db_conn);   
         }
-
-        handleResetInterviewRequest() {
-                    $interviewTuple1 = array (":bind1" => "Elon Musk", ":bind2" => "Sean", ":bind3" => "120923");
-                    $interviewTuple2 = array (":bind1" => "Steve Jobs", ":bind2" => "Aaron", ":bind3" => "121023");
-                    $interviewTuple3 = array (":bind1" => "Bill Gates", ":bind2" => "Gittu", ":bind3" => "120923");
-                    $interviewTuple4 = array (":bind1" => "Elon Musk", ":bind2" => "Dani", ":bind3" => "121023");
-                    $interviewTuple5 = array (":bind1" => "Steve Jobs", ":bind2" => "Yan", ":bind3" => "122123");
-                    $allInterviewtuples = array ($interviewTuple1, $interviewTuple2, $interviewTuple3, $interviewTuple4, $interviewTuple5);
-                    global $db_conn;
-                    // Drop old table
-                    executePlainSQL("DROP TABLE interviewTable");
-                    echo "<br> old interview table dropped <br>";
-                    // Create new table
-                    echo "<br> creating new interview table <br>";
-                    executePlainSQL("CREATE TABLE interviewTable (interviewer char(30), interviewee char(30), date int, PRIMARY KEY(interviewer, interviewee, date))");
-                    echo "<br> new interviewTable created <br>";
-                    executeBoundSQL("insert into interviewTable values (:bind1, :bind2, :bind3)", $allInterviewtuples);
-                    echo "<br> default tuples inserted <br>";
-                    OCICommit($db_conn);
-                    }
-
-
+        
         function handleInsertRequest() {
             global $db_conn;
 
@@ -354,7 +275,7 @@
             OCICommit($db_conn);
         }
 
-
+        
         function handleInsertJobRequest() {
             global $db_conn;
 
@@ -374,27 +295,6 @@
             executeBoundSQL("insert into jobTable values (:bind1, :bind2, :bind3, :bind4, :bind5)", $alltuples);
             OCICommit($db_conn);
         }
-
-
-        function handleInsertInterviewRequest() {
-                    global $db_conn;
-
-                    //Getting the values from user and insert data into the table
-                    $tuple = array (
-                        ":bind1" => $_POST['insInt'],
-                        ":bind2" => $_POST['insIntee'],
-                        ":bind3" => $_POST['insDate'],
-                    );
-
-                    $alltuples = array (
-                        $tuple
-                    );
-
-                    executeBoundSQL("insert into interviewTable values (:bind1, :bind2, :bind3)", $alltuples);
-                    OCICommit($db_conn);
-                }
-
-
         function handleUpdateRequest() {
             //ob_end_clean();
             global $db_conn;
@@ -428,21 +328,6 @@
             $result = executePlainSQL("SELECT * FROM jobTable");
             printResultJob($result);
         }
-
-
-        function handleCountInterviewRequest() {
-                    global $db_conn;
-
-                    $result = executePlainSQL("SELECT Count(*) FROM interviewTable");
-
-                    if (($row = oci_fetch_row($result)) != false) {
-                        echo "<br> The number of tuples in interviewTable: " . $row[0] . "<br>";
-                    }
-                    $result = executePlainSQL("SELECT * FROM interviewTable");
-                    printResultInterview($result);
-                }
-
-
         // HANDLE ALL POST ROUTES
 	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
         function handlePOSTRequest() {
@@ -457,12 +342,7 @@
                     handleInsertRequest();
                 } else if (array_key_exists('insertJobQueryRequest', $_POST)) {
                     handleInsertJobRequest();
-                 // new
-                } else if (array_key_exists('resetInterviewTablesRequest', $_POST)) {
-                    handleResetInterviewRequest();
-                } else if (array_key_exists('insertInterviewQueryRequest', $_POST)) {
-                    handleInsertInterviewRequest();
-             }
+                }
 
                 disconnectFromDB();
             }
@@ -476,10 +356,8 @@
                     handleCountRequest();
                 } else if (array_key_exists('printJob', $_GET)) {
                     handleCountJobRequest();
-                // NEW
-                } else if (array_key_exists('printInterview', $_GET)) {
-                    handleCountInterviewRequest();
                 }
+
                 disconnectFromDB();
             }
         }
@@ -488,9 +366,6 @@
             handlePOSTRequest();
         } else if (isset($_GET['countTupleRequest']) || isset($_GET['printRequest'])) {
             handleGETRequest();
-            } // new
-                    else if (isset($_POST['resetInterview']) || isset($_POST['reset']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit']) || isset($_POST['insertSubmitInterview'])) {
-                        handlePOSTRequest();
         }
 
         function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
@@ -552,7 +427,7 @@
                 }
             }
         }
-
+        print_r($_GET);
         ?>
     </body>
 </html>
