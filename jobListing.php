b0ed203fe1c1e0613b393e38fb01e2e531c6db34
Fulino24k <liusean24@outlook.com>
2023-08-04 23:17:22 -0700

    non-meaningful commit for update purposes with some structural changes

--- jobListing.php
+++ jobListing.php
@@ -3,56 +3,77 @@
         <title> CPSC 304 2023S T2 PHP/Oracle Project</title>
     </head>
     <style>
+        <?php include 'project.css'; ?>
     </style>
     <body>
         <h1> Application For Job </h1>
-        <p>pick a option below to get started with</p><hr />
-
-        <?php
-            if ($c=OCILogon("ora_fulino", "a72201767", "dbhost.students.cs.ubc.ca:1522/stu")) {
-                echo "Successfully connected to Oracle.\n";
-                OCILogoff($c);
-            } else {
-                $err = OCIError();
-                echo "Oracle Connect Error " . $err['message'];
-            }
-        ?>
+        <hr/>
         <script>
-            var show = false;
-            function appliesFor() {
-                var x = document.getElementById("reset");
-                var y = document.getElementById("resetJob");
-                var a = document.getElementById("insert");
-                var b = document.getElementById("insertJob");
-                if (x.style.display === "none") {
-                    if (show) {
-                        a.style.display = "none";
-                        b.style.display = "none";
-                    }
-                    x.style.display = "block";
-                    y.style.display = "block";
-                } else {
-                    x.style.display = "none";
-                }
-                show = true;
+            function applyFor() {
+                var a = document.getElementById("coverResume");
+                var b = document.getElementById("resume");
+                var c = document.getElementById("edit");
+                a.style.display = "block";
+                b.style.display = "block";
+                c.style.display = "block";
             }
         </script>
-
         <?php
-        if (isset($_POST['resetJob']) || isset($_POST['reset']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit']) || isset($_POST['insertSubmitJob'])) {
+        include 'functions.php';
+        $positionID = $_GET['posID'];
+        function handlePOSTRequest() {
+            if (connectToDB()) {
+                if (array_key_exists('coverRequest', $_POST)) {
+                    handleCoverRequest();
+                } else if (array_key_exists('resetTablesRequest', $_POST)) {
+                    handleResetRequest();
+                } disconnectFromDB();
+            }
+        }
+        if (isset($_POST['Save']) || isset($_POST['reset'])){
             handlePOSTRequest();
+            echo "handling post request";
         } else if (isset($_GET['posID'])) {
             handleGETRequest();
+        } 
+        function handleCoverRequest() {
+
+            global $db_conn;
+            $tuple = array (
+                ":bind1" => $_POST['cover'],
+                ":bind2" => $_POST['res']
+            );
+
+            $alltuples = array (
+                $tuple
+            );
+
+            executeBoundSQL("insert into coverTable values (:bind1, :bind2)", $alltuples);
+            OCICommit($db_conn);
         }
+        function handleResetRequest() {
+            global $db_conn;
+            // Drop old table
+            executePlainSQL("DROP TABLE coverTable");
+            echo "<br> old table dropped <br>";
+            // Create new table
+            echo "<br> creating new table <br>";
+                executePlainSQL("CREATE TABLE storeTable (app_num int PRIMARY KEY, apply_date int)");
+                executePlainSQL("CREATE TABLE coverTable (app_num int PRIMARY KEY, introduction char(300))");
+                executePlainSQL("CREATE TABLE resumeTable (app_num int PRIMARY KEY, name char(30), experience char(300), education char(300))");
+            OCICommit($db_conn);
+        }
+        // $positionID = $_GET['posID'];
+
+        // echo $positionID;
         function handleJobRequest() {
             global $db_conn;
             $positionID = $_GET['posID'];
-
             $resultName = executePlainSQL("SELECT * FROM jobTable WHERE referenceID = $positionID");
             //echo "For Applying For Position: ". $resultName['POSITION'] . "<br>";
             while ($row = OCI_Fetch_Array($resultName, OCI_BOTH)) {
                 echo "<br>";
-                echo "For Applying For Position: " . $row["POSITION"] . "<br><br>"; //or just use "echo $row[0]"
+                echo "Applying For Position: " . $row["POSITION"] . "<br><br>"; //or just use "echo $row[0]"
                 echo "The qualifications for this job: " . $row["QUALIFICATION"] . "<br>";
                 echo "The duties for this job: " . $row["DUTY"] . "<br>";
             }
@@ -64,100 +85,63 @@
             if (connectToDB()) {
                 if (array_key_exists('posID', $_GET)) {
                     handleJobRequest();
-                } 
+                } else {
+                    handleJobRequest();
+                }
                 disconnectFromDB();
             }
         }
-        function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-            //echo "<br>running ".$cmdstr."<br>";
-            global $db_conn, $success;
+        
+        ?>
+        <!-- <div id="group" class="application">
+            <button onclick="applyFor()">New Application</button>
+            <button>Load Application</button>
+        </div> -->
+        <div id="group" class="application">
+            <button onclick="applyFor()">New Application</button>
+            <button>Load Application</button>
+        </div>
+        
+        <form id="buttons" method="POST" action="jobListing.php">
+            <input type="hidden" id="showApp" name="showApp">
+            <input type="button" name="new" value="New Application" />
+            <input type="button" name="load" value="Load Application" />
+        </form>
 
-            $statement = OCIParse($db_conn, $cmdstr);
-            //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+        <form id="buttons" method="GET" action="jobListing.php" name="fresh">
+            <input type="hidden" id="yeet" name="posID" value="$_GET['posID']">
+        </form>
+        
+        <!-- <input type="hidden" id="submitJob" name="submitJob"> -->
+        <form id="coverResume"  method="POST" style="display: none" action="jobListing.php"> 
+            <p>Cover Letter:</p>
+            <input type="hidden" id="appRequest" name="appRequest">
+            <input type="text" style="height:100px; width:350px;" name="cover"><br><br>
+            <p>Resume:</p>
+            <input type="text" style="height:100px; width:350px;" name="res"><br><br>
+            <input type="button" value="Save" name="saveApp">
+            <input type="button" value="Edit" name="editApp"> 
+            <input type="submit" value="submit" name="submitApp">
+        </form>
+        
+        <form id="reset" method="POST" action="project.php">
+            <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
+            <p><input type="submit" value="Reset Cover + Resume + Stores" name="reset"></p>
+        </form>
 
-            if (!$statement) {
-                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-                $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-
-            $r = OCIExecute($statement, OCI_DEFAULT);
-            if (!$r) {
-                echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-
-            return $statement;
+        <?php
+        if(array_key_exists('new', $_POST)) {
+            newApp();
+        } else if(array_key_exists('load', $_POST)) {
+            loadApp();
+        } else if(array_key_exists('showApp', $_POST)) {
+            loadApp();
         }
-
-        function executeBoundSQL($cmdstr, $list) {
-            /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
-        In this case you don't need to create the statement several times. Bound variables cause a statement to only be
-        parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
-        See the sample code below for how this function is used */
-
-            global $db_conn, $success;
-            $statement = OCIParse($db_conn, $cmdstr);
-
-            if (!$statement) {
-                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-                $e = OCI_Error($db_conn);
-                echo htmlentities($e['message']);
-                $success = False;
-            }
-
-            foreach ($list as $tuple) {
-                foreach ($tuple as $bind => $val) {
-                    //echo $val;
-                    //echo "<br>".$bind."<br>";
-                    OCIBindByName($statement, $bind, $val);
-                    unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-                }
-
-                $r = OCIExecute($statement, OCI_DEFAULT);
-                if (!$r) {
-                    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                    $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
-                    echo htmlentities($e['message']);
-                    echo "<br>";
-                    $success = False;
-                }
-            }
+        function newApp() {
+            echo "This is Button1 that is selected";
         }
-        function connectToDB() {
-            global $db_conn;
-
-            // Your username is ora_(CWL_ID) and the password is a(student number). For example,
-			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_fulino", "a72201767", "dbhost.students.cs.ubc.ca:1522/stu");
-
-            if ($db_conn) {
-                debugAlertMessage("Database is Connected");
-                return true;
-            } else {
-                debugAlertMessage("Cannot connect to Database");
-                $e = OCI_Error(); // For OCILogon errors pass no handle
-                echo htmlentities($e['message']);
-                return false;
-            }
-        }
-
-        function debugAlertMessage($message) {
-            global $show_debug_alert_messages;
-
-            if ($show_debug_alert_messages) {
-                echo "<script type='text/javascript'>alert('" . $message . "');</script>";
-            }
-        }
-
-        function disconnectFromDB() {
-            global $db_conn;
-
-            debugAlertMessage("Disconnect from Database");
-            OCILogoff($db_conn);
+        function loadApp() {
+            echo "This is Button2 that is selected";
         }
         ?>
     </body>
