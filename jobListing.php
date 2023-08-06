ea66fd57df9d1190e4977f318c99efc7f408a35c
Fulino24k <liusean24@outlook.com>
2023-08-05 22:20:22 -0700

    functional job application portal now

--- jobListing.php
+++ jobListing.php
@@ -9,6 +9,12 @@
         <h1> Application For Job </h1>
         <hr/>
         <script>
+            var hide = FALSE;
+            var condition = sessionStorage.getItem("key");
+            if (condition) {
+                var b = document.getElementById("group");
+                b.style.display = "none";
+            }
             function applyFor() {
                 var a = document.getElementById("coverResume");
                 var b = document.getElementById("resume");
@@ -16,6 +22,16 @@
                 a.style.display = "block";
                 b.style.display = "block";
                 c.style.display = "block";
+            }
+            function end() {
+                var b = document.getElementById("group");
+                b.style.display = "none";
+                hide = TRUE;
+                sessionStorage.setItem("key", hide);
+                var frm = document.getElementsByName('coverResume');
+                frm.submit(); // Submit the form
+                frm.reset();  // Reset all form data
+                return false;
             }
         </script>
         <?php
@@ -27,7 +43,10 @@
                     handleCoverRequest();
                 } else if (array_key_exists('resetTablesRequest', $_POST)) {
                     handleResetRequest();
-                } disconnectFromDB();
+                } else if (array_key_exists('submitApp', $_POST)) {
+                    handleSubmitRequest();
+                } 
+                disconnectFromDB();
             }
         }
         if (isset($_POST['Save']) || isset($_POST['reset'])){
@@ -36,41 +55,17 @@
         } else if (isset($_GET['posID'])) {
             handleGETRequest();
         } 
-        function handleCoverRequest() {
-
-            global $db_conn;
-            $tuple = array (
-                ":bind1" => $_POST['cover'],
-                ":bind2" => $_POST['res']
-            );
-
-            $alltuples = array (
-                $tuple
-            );
-
-            executeBoundSQL("insert into coverTable values (:bind1, :bind2)", $alltuples);
-            OCICommit($db_conn);
+        if (isset($_POST['appRequest'])) {
+            handlePOSTRequest();
+        } else if (isset($_GET['printRequest']) || isset($_GET['printRequestInterview']) || isset($_GET['printRequestAccount'])) {
+            handleGETRequest();
         }
-        function handleResetRequest() {
-            global $db_conn;
-            // Drop old table
-            executePlainSQL("DROP TABLE coverTable");
-            echo "<br> old table dropped <br>";
-            // Create new table
-            echo "<br> creating new table <br>";
-                executePlainSQL("CREATE TABLE storeTable (app_num int PRIMARY KEY, apply_date int)");
-                executePlainSQL("CREATE TABLE coverTable (app_num int PRIMARY KEY, introduction char(300))");
-                executePlainSQL("CREATE TABLE resumeTable (app_num int PRIMARY KEY, name char(30), experience char(300), education char(300))");
-            OCICommit($db_conn);
-        }
-        // $positionID = $_GET['posID'];
 
-        // echo $positionID;
         function handleJobRequest() {
             global $db_conn;
             $positionID = $_GET['posID'];
             $resultName = executePlainSQL("SELECT * FROM jobTable WHERE referenceID = $positionID");
-            //echo "For Applying For Position: ". $resultName['POSITION'] . "<br>";
+
             while ($row = OCI_Fetch_Array($resultName, OCI_BOTH)) {
                 echo "<br>";
                 echo "Applying For Position: " . $row["POSITION"] . "<br><br>"; //or just use "echo $row[0]"
@@ -85,63 +80,73 @@
             if (connectToDB()) {
                 if (array_key_exists('posID', $_GET)) {
                     handleJobRequest();
-                } else {
-                    handleJobRequest();
-                }
+                } 
                 disconnectFromDB();
             }
         }
         
         ?>
-        <!-- <div id="group" class="application">
-            <button onclick="applyFor()">New Application</button>
-            <button>Load Application</button>
-        </div> -->
         <div id="group" class="application">
             <button onclick="applyFor()">New Application</button>
             <button>Load Application</button>
         </div>
         
-        <form id="buttons" method="POST" action="jobListing.php">
+        <!-- <form id="buttons" method="POST" action="jobListing.php">
             <input type="hidden" id="showApp" name="showApp">
             <input type="button" name="new" value="New Application" />
             <input type="button" name="load" value="Load Application" />
-        </form>
+        </form> -->
 
         <form id="buttons" method="GET" action="jobListing.php" name="fresh">
             <input type="hidden" id="yeet" name="posID" value="$_GET['posID']">
         </form>
-        
+        <br>
         <!-- <input type="hidden" id="submitJob" name="submitJob"> -->
-        <form id="coverResume"  method="POST" style="display: none" action="jobListing.php"> 
+        <form id="coverResume"  method="POST" style="display: none" > 
             <p>Cover Letter:</p>
             <input type="hidden" id="appRequest" name="appRequest">
-            <input type="text" style="height:100px; width:350px;" name="cover"><br><br>
+            <input type="text" style="height:100px; width:350px;" name="insCover"><br><br>
             <p>Resume:</p>
-            <input type="text" style="height:100px; width:350px;" name="res"><br><br>
+            <p>Name: </p> 
+            <input type="text" name="insName"> 
+            <p>Experience:</p>
+            <input type="text" style="height:100px; width:350px;" name="insExp">
+            <p>Education:</p>
+            <input type="text" style="height:100px; width:350px;" name="insEdu"><br><br>
             <input type="button" value="Save" name="saveApp">
             <input type="button" value="Edit" name="editApp"> 
-            <input type="submit" value="submit" name="submitApp">
-        </form>
-        
-        <form id="reset" method="POST" action="project.php">
-            <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
-            <p><input type="submit" value="Reset Cover + Resume + Stores" name="reset"></p>
+            <input type="submit" value="submit" name="submitApp" >
         </form>
 
         <?php
-        if(array_key_exists('new', $_POST)) {
-            newApp();
-        } else if(array_key_exists('load', $_POST)) {
-            loadApp();
-        } else if(array_key_exists('showApp', $_POST)) {
-            loadApp();
-        }
-        function newApp() {
-            echo "This is Button1 that is selected";
-        }
-        function loadApp() {
-            echo "This is Button2 that is selected";
+        function handleSubmitRequest() {
+            global $db_conn;
+
+            //Getting the values from user and insert data into the table
+            $tupleCover = array (
+                ":bind1" => "333",
+                ":bind2" => $_POST['insCover']
+            );
+
+            $tupleResume = array (
+                ":bind1" => "333",
+                ":bind2" => $_POST['insName'],
+                ":bind3" => $_POST['insExp'],
+                ":bind4" => $_POST['insEdu'],
+            );
+
+            $cover = array (
+                $tupleCover
+            );
+
+            $resume = array (
+                $tupleResume
+            );
+
+            executeBoundSQL("insert into coverTable values (:bind1, :bind2)", $cover);
+            executeBoundSQL("insert into resumeTable values (:bind1, :bind2, :bind3, :bind4)", $resume);
+            OCICommit($db_conn);
+            echo "Application Submitted, Thank You!";
         }
         ?>
     </body>
